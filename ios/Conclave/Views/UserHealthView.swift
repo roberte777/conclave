ee253264d956fb96c35741ad201129733a8ac074
colbydeason <colbydeason@gmail.com>
2025-07-27 16:47:12 -0500

    add hold press and prevent app from rotating.

--- ios/Conclave/Views/UserHealthView.swift
+++ ios/Conclave/Views/UserHealthView.swift
@@ -25,6 +25,38 @@ struct UserHealthView: View {
     struct HealthButtons: View {
         @Binding var givenHealthTotal: Int
         @Binding var givenLifeOrientation: LifeOrientation
+        @State private var leftHoldTimer: Timer?
+        @State private var rightHoldTimer: Timer?
+
+        private func subAddStartTimer(isLeft: Bool) {
+            if isLeft {
+                givenHealthTotal -= 10
+                leftHoldTimer = Timer.scheduledTimer(
+                    withTimeInterval: 0.5,
+                    repeats: true
+                ) { _ in
+                    givenHealthTotal -= 10
+                }
+            } else {
+                givenHealthTotal += 10
+                rightHoldTimer = Timer.scheduledTimer(
+                    withTimeInterval: 0.5,
+                    repeats: true
+                ) { _ in
+                    givenHealthTotal += 10
+                }
+            }
+        }
+
+        private func stopTimer(isLeft: Bool) {
+            if isLeft {
+                leftHoldTimer?.invalidate()
+                leftHoldTimer = nil
+            } else {
+                rightHoldTimer?.invalidate()
+                rightHoldTimer = nil
+            }
+        }
 
         var body: some View {
             let flipLeftRight =
@@ -44,6 +76,18 @@ struct UserHealthView: View {
                             givenHealthTotal -= 1
                         }
                     }
+                    .gesture(
+                        LongPressGesture(minimumDuration: 0.5)
+                            .onEnded { _ in
+                                subAddStartTimer(isLeft: !flipLeftRight)
+                            }
+                            .simultaneously(
+                                with: DragGesture(minimumDistance: 0)
+                            )
+                            .onEnded { _ in
+                                stopTimer(isLeft: !flipLeftRight)
+                            }
+                    )
                 Image(systemName: flipLeftRight ? "plus" : "minus")
                     .rotationEffect(Angle(degrees: flipAngle))
                     .foregroundColor(.white)
@@ -61,6 +105,18 @@ struct UserHealthView: View {
                             givenHealthTotal += 1
                         }
                     }
+                    .gesture(
+                        LongPressGesture(minimumDuration: 0.5)
+                            .onEnded { _ in
+                                subAddStartTimer(isLeft: flipLeftRight)
+                            }
+                            .simultaneously(
+                                with: DragGesture(minimumDistance: 0)
+                            )
+                            .onEnded { _ in
+                                stopTimer(isLeft: flipLeftRight)
+                            }
+                    )
                 Image(systemName: flipLeftRight ? "minus" : "plus")
                     .rotationEffect(Angle(degrees: flipAngle))
                     .foregroundColor(.white)
